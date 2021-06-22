67c4bd4e01757455fffbf5ad20557406069e6a67
WillFontana <willyanfontana@gmail.com>
2021-06-22 15:46:03 -0300

    Implementa tela home e header do perfil

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 
 import { Background } from "./src/components/Background";
-import { SignIn } from "./src/screens/SignIn";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_500Medium, Rajdhani_500Medium, Rajdhani_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      < SignIn />
+      <Home />
     </Background>
 
   ) : <AppLoading />;
