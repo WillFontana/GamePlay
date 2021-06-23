5b34ff692adef9e3e3815c3ff4db98c2ce014601
WillFontana <willyanfontana@gmail.com>
2021-06-23 13:12:12 -0300

    Desenvolve modal de escolhas de guilds

---
+++ src/screens/Guilds/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, FlatList } from "react-native";
+import { Guild, GuildProps } from "../../components/Guild";
+import { ListDivider } from "../../components/ListDivider";
+
+import { styles } from "./styles";
+
+type Props = {
+  handleGuildSelect: (guild: GuildProps) => void;
+};
+
+export function Guilds({ handleGuildSelect }: Props) {
+  const guilds = [
+    { id: "1", name: "A Disney", icon: "image.png", owner: true },
+    { id: "2", name: "Irmandade da Luz", icon: "image.png", owner: true },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={guilds}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Guild onPress={() => handleGuildSelect(item)} data={item} />
+        )}
+        ItemSeparatorComponent={ListDivider}
+        showsVerticalScrollIndicator={false}
+        style={styles.guilds}
+      />
+    </View>
+  );
+}
